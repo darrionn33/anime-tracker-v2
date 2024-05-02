654f1911e7dfda1e623a13243978ec28c534ece3
darrionn33 <darrionn33@gmail.com>
2024-05-02 13:40:34 +0530

    added missing theme color in vite config

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,13 @@ import { VitePWA } from "vite-plugin-pwa";
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/anime-tracker-v2",
-  plugins: [react(), VitePWA({ registerType: "autoUpdate" })],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        theme_color: "#302674",
+      },
+    }),
+  ],
 });
